902591cb25a3a7a7be5e4190650cc3ad7191e9d4
Lam Tran <lam.tran@commercetools.de>
2017-11-25 10:58:47 +0100

    unnamed module

--- src/com/commercetools/importer/Main.java
+++ src/com/commercetools/importer/Main.java
@@ -4,10 +4,15 @@ import com.commercetools.importer.product.ProductsQueryService;
 import com.commercetools.importer.product.ProductsQueryServiceImpl;
 import com.commercetools.importer.product.impl.Product;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 class Main {
+
     public static void main(String[] args) {
         ProductsQueryService service = new ProductsQueryServiceImpl();
         Product product = service.getProductByKey("test");
-        System.out.println(product.getName());
+        Logger logger = Logger.getGlobal();
+        logger.log(Level.INFO, product.getName());
     }
 }
