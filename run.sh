212340e044ba080fe808c83628cf6dd066658453
lojzatran <lojza.tran@tradesports.com>
2017-12-03 19:16:11 +0100

    jllink

--- run.sh
+++ run.sh
@@ -7,8 +7,14 @@
 
 /Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/javac -d out/production/product-graphql-query   src/product-graphql-query/com/commercetools/importer/product/graphql/impl/ProductGraphQLQueryServiceImpl.java src/product-graphql-query/module-info.java  -p out/production/product-api:out/production/domain
 
-/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/javac -classpath lib/jackson-core-2.9.0.jar -d out/production/runner -p out/production/product-api:out/production/domain:src/runner:mods src/runner/module-info.java src/runner/com/commercetools/importer/Main.java src/runner/com/commercetools/importer/utils/ConfigurationUtils.java src/runner/com/commercetools/importer/utils/ProjectConfiguration.java src/runner/com/commercetools/importer/utils/TenantConfiguration.java
+/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/javac -d out/production/runner -p out/production/product-api:out/production/domain:src/runner:mods src/runner/module-info.java src/runner/com/commercetools/importer/Main.java
 
-/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/javac --patch-module product.projections.query=src/product-projections-query/test --module-path out/production/product-api:out/production/domain:out/production/product-projections-query:lib --add-modules junit --add-reads product.projections.query=junit -d out-test $(find src/product-projections-query/test -name *.java)
+/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/jar -cf mods/domains.jar -C out/production/domain .
 
-/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/java --patch-module product.projections.query=out-test --add-reads product.projections.query=junit --module-path out/production/product-api:out/production/domain:out/production/product-projections-query:lib --add-modules product.projections.query --add-opens product.projections.query/com.commercetools.importer.product.projections.impl=junit -m junit/org.junit.runner.JUnitCore com.commercetools.importer.product.projections.impl.ProductProjectionsQueryServiceImplTest
+/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/jar -cf mods/product-api.jar -C out/production/product-api .
+
+/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/jar -cf mods/product-projections.jar -C out/production/product-projections-query .
+
+/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/jar -cf mods/runner.jar  -C out/production/runner .
+
+/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/bin/jlink --module-path mods/:/Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home/jmods --add-modules com.commercetools.importer --add-modules product.projections.query --launcher importer=com.commercetools.importer/com.commercetools.importer.Main --output image
--- src/runner/com/commercetools/importer/Main.java
+++ src/runner/com/commercetools/importer/Main.java
@@ -2,11 +2,8 @@ package com.commercetools.importer;
 
 import com.commercetools.importer.domains.Product;
 import com.commercetools.importer.product.ProductsQueryService;
-import com.commercetools.importer.utils.ConfigurationUtils;
-import com.commercetools.importer.utils.ProjectConfiguration;
 
 import java.io.IOException;
-import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,16 +12,12 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         Logger logger = Logger.getGlobal();
-        Optional<ProjectConfiguration> configuration = ConfigurationUtils.getConfiguration("/Users/ltran/Projects/ctp-java-9-modules/src/runner/com/commercetools/importer/config.json");
-
-        logger.log(Level.INFO, "config is " + configuration.get().toString());
+        logger.log(Level.INFO, "Starting the importer module");
 
         ServiceLoader<ProductsQueryService> serviceLoader = ServiceLoader.load(ProductsQueryService.class);
         serviceLoader.forEach(productsQueryService -> {
             Product product = productsQueryService.getProductByKey("testKey");
             logger.log(Level.INFO, product.getName());
         });
-
-
     }
 }
